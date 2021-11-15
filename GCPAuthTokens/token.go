2f887198b18aa27c9701ebafc93694ab133eb237
Farsos Bulsara <16597052+kingsukhoi@users.noreply.github.com>
2021-11-15 10:24:15 -0700

    Add ability to get auth tokens from GCP.
    This is to enable the ability to connect to Cloudsql using IAM accounts

---
+++ GCPAuthTokens/token.go
@@ -0,0 +1,80 @@
+package pkg
+
+import (
+	credentials "cloud.google.com/go/iam/credentials/apiv1"
+	"context"
+	"google.golang.org/api/option"
+	credentialsPb "google.golang.org/genproto/googleapis/iam/credentials/v1"
+	"os"
+)
+
+const (
+	EnvServiceAcctFile = "GOOGLE_APPLICATION_CREDENTIALS"
+)
+
+func isEnvExist(key string) bool {
+	if _, ok := os.LookupEnv(key); ok {
+		return true
+	}
+	return false
+}
+
+func GetAuthToken(saEmail string) (string, error) {
+	if isEnvExist(EnvServiceAcctFile) {
+		return GetAuthFromFile(os.Getenv(EnvServiceAcctFile), saEmail)
+	} else {
+		return GetAuthFromKube(saEmail)
+	}
+
+}
+
+func GetAuthFromKube(saEmail string) (string, error) {
+	ctx := context.Background()
+	c, err := credentials.NewIamCredentialsClient(ctx)
+	if err != nil {
+		return "", err
+	}
+	defer c.Close()
+
+	requestOpts := &credentialsPb.GenerateAccessTokenRequest{
+		Name:  saEmail,
+		Scope: []string{"https://www.googleapis.com/auth/cloud-platform"},
+	}
+
+	token, err := c.GenerateAccessToken(ctx, requestOpts)
+
+	if err != nil {
+		return "", err
+	}
+
+	return token.AccessToken, nil
+}
+
+func GetAuthFromFile(Path string, saEmail string) (string, error) {
+	ctx := context.Background()
+	if _, err := os.Stat(Path); os.IsNotExist(err) {
+		return "", err
+	} else if err != nil {
+		return "", err
+	}
+
+	c, err := credentials.NewIamCredentialsClient(ctx, option.WithCredentialsFile(Path))
+	if err != nil {
+		return "", err
+	}
+
+	defer c.Close()
+
+	requestOpts := &credentialsPb.GenerateAccessTokenRequest{
+		Name:  saEmail,
+		Scope: []string{"https://www.googleapis.com/auth/cloud-platform"},
+	}
+
+	token, err := c.GenerateAccessToken(ctx, requestOpts)
+
+	if err != nil {
+		return "", err
+	}
+
+	return token.AccessToken, nil
+}
--- go.mod
+++ go.mod
@@ -3,18 +3,35 @@ module github.com/Flahmingo-Investments/helpers-go
 go 1.17
 
 require (
+	cloud.google.com/go v0.97.0
 	github.com/stretchr/testify v1.7.0
 	go.uber.org/zap v1.19.0
+	google.golang.org/api v0.60.0
 	google.golang.org/genproto v0.0.0-20211029142109-e255c875f7c7
 	google.golang.org/grpc v1.41.0
 )
 
 require (
+	github.com/census-instrumentation/opencensus-proto v0.2.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/cncf/udpa/go v0.0.0-20201120205902-5459f2c99403 // indirect
+	github.com/cncf/xds/go v0.0.0-20210805033703-aa0b78936158 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/envoyproxy/go-control-plane v0.9.10-0.20210907150352-cf90f659a021 // indirect
+	github.com/envoyproxy/protoc-gen-validate v0.1.0 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/googleapis/gax-go/v2 v2.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	go.opencensus.io v0.23.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
+	golang.org/x/net v0.0.0-20210503060351-7fd8e65b6420 // indirect
+	golang.org/x/oauth2 v0.0.0-20211005180243-6b3c2da341f1 // indirect
+	golang.org/x/sys v0.0.0-20211025201205-69cdffdb9359 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
