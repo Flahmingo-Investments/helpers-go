f44ab4c67ac0489df0b30e89195df0dcaf96fca0
Farsos Bulsara <16597052+kingsukhoi@users.noreply.github.com>
2021-12-13 16:47:49 -0700

    Add example for using sqlx and IAM accounts

---
+++ examples/dbAccess/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Flahmingo-Investments/helpers-go/flog"
+	"github.com/Flahmingo-Investments/helpers-go/gcpauth"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	const saEmail string = "farsos-test@development-flahmingo.iam"
+	token, err := gcpauth.GetAuthToken(saEmail)
+	if err != nil {
+		flog.Errorf("Failed to get auth token", err)
+		return
+	}
+
+	connString := fmt.Sprintf(
+		"user=%s dbname=postgres sslmode=disable password=%s",
+		saEmail, token)
+
+	//this automatically runs db.ping()
+	conn, err := sqlx.Connect("postgres", connString)
+	if err != nil {
+		flog.Errorf("DB conn failed", err)
+	}
+	flog.Infof("Successful connection")
+	defer conn.Close()
+}
