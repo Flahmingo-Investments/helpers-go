b6f5aeb1caf8fc23036341a1d7585a805511ac16
Farsos Bulsara <16597052+kingsukhoi@users.noreply.github.com>
2021-12-13 16:47:22 -0700

    Add getting secret from secert manager code.
    Also added a test

---
+++ gcpauth/test/secretManager_test.go
@@ -0,0 +1,20 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/Flahmingo-Investments/helpers-go/gcpauth"
+)
+
+// This test assumes that either gcloud is setup or you are working inside of
+func TestGetSecretByName(t *testing.T) {
+	secret, err := gcpauth.GetSecretByName("***REMOVED***")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if secret != "test-value" {
+		t.Error("wrong value")
+	}
+}
