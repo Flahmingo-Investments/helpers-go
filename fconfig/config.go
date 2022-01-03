0fc9efc3c27cf78b9243f5a83bb7b10ffa1d29fb
Farsos Yazdi Bulsara <16597052+kingsukhoi@users.noreply.github.com>
2022-01-03 10:17:02 -0700

    Add a config parser to helpers library (#9)
    * Initial commit of Config parser
    
    * Remove unnecessary os.stat call
    
    Also go lint fixes
    
    * Stop the test
    
    * Remove viper env setup
    
    * Fix testing bug
    
    The nested value test passed because the default value for a bool is false, so it made the condition pass.
    Made the values public, and if there ever is an issue with it, the test will catch it
    
    * Moved assets to testdata.
    
    Also removed the export from the env file.
    
    * Cleaned up as per code review
    
    * Fix golint issue
    
    * Re configure for single config file and import a local .env file
    
    * Re configure for single config file and import a local .env file
    
    * Go Lint fixes
    
    * feat: add gcp secret manager helper
    
    * chore: remove gcpauth
    
    * refactor: integrate secret manager
    
    Co-authored-by: Arman <hello@arman.work>

---
+++ fconfig/config.go
@@ -0,0 +1,129 @@
+package fconfig
+
+import (
+	"os"
+	"regexp"
+
+	"github.com/Flahmingo-Investments/helpers-go/ferrors"
+	"github.com/Flahmingo-Investments/helpers-go/flog"
+	"github.com/Flahmingo-Investments/helpers-go/gcp"
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
+)
+
+var secretRegex = regexp.MustCompile(`^gSecret://(?P<Path>.+)`)
+
+// secretClient is helper to expand gcp.SecretClient to support gSecret in path.
+type secretClient struct {
+	*gcp.SecretClient
+}
+
+// getSecret parses a `gSecret://` string into a GCP secret path, and retrieve
+// it from GCP Secret Service.
+func (c *secretClient) getSecret(val string) (string, error) {
+	matches := secretRegex.FindStringSubmatch(val)
+	pathIndex := secretRegex.SubexpIndex("Path")
+	path := matches[pathIndex]
+	return c.GetSecret(path)
+}
+
+// loadConfig loads the configuration from a given file.
+//
+// It expands the environment variables if the value matches `${ENV_NAME}`.
+// It fetches the secret if the value matches gSecret://uri.
+func loadConfig(file string, config interface{}) error {
+	// initialize viper and set the config file to read from.
+	v := viper.New()
+	v.SetConfigFile(file)
+
+	// initialize GCP Secret Manager Client
+	gsc, err := gcp.NewSecretClient()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		_ = gsc.Close()
+	}()
+
+	// wrap gsc into secretClient to support `gSecret://` expansion.
+	sc := secretClient{SecretClient: gsc}
+
+	// Read the configuration.
+	err = v.ReadInConfig()
+	if err != nil {
+		return err
+	}
+
+	// Range over all keys and do the following:
+	// - Expand if it is environment variable.
+	// - Fetch if it is gSecret.
+	for _, key := range v.AllKeys() {
+		val := v.GetString(key)
+		if val == "" {
+			continue
+		}
+		// expand environment variables inside config file
+		// e.g. ${ENV_NAME}
+		v.Set(key, os.ExpandEnv(val))
+
+		// fetch the secret if value matches the secretRegex
+		if secretRegex.MatchString(val) {
+			flog.Debugf("matched: %s", val)
+			secret, err := sc.getSecret(val)
+			if err != nil {
+				return err
+			}
+
+			flog.Debugf("secret: %s", secret)
+			v.Set(key, secret)
+		}
+	}
+
+	return v.Unmarshal(config)
+}
+
+// LoadConfig loads the configuration from a given file and unmarshal it into
+// the provided config.
+// It maps the fields using `mapstructure` tag.
+//
+// Example:
+//   type Config struct {
+//   	 Env   string `mapstructure:env`
+//   	 DBUri string `mapstructure:db_uri`
+//   }
+//
+// It expands the environment variables if the value matches `${ENV_NAME}`.
+// It fetches the secret if the value matches gSecret://uri.
+func LoadConfig(file string, config interface{}) error {
+	// load .env file inside the current working directory.
+	err := LoadEnv("")
+	if err != nil {
+		return ferrors.Wrap(err, "unable to read environment variables")
+	}
+
+	// load configuration into the provided config.
+	err = loadConfig(file, config)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// LoadEnv load environments variables from a file.
+// If no file name is given it will try to load .env file.
+func LoadEnv(filename string) error {
+	var err error
+
+	if filename != "" {
+		err = godotenv.Load(filename)
+	} else {
+		err = godotenv.Load()
+		// it is fine if .env does not exists
+		if os.IsNotExist(err) {
+			return nil
+		}
+	}
+
+	return ferrors.WithStack(err)
+}
---
+++ fconfig/config_test.go
@@ -0,0 +1,75 @@
+//go:build integration
+// +build integration
+
+package fconfig
+
+import (
+	"reflect"
+	"testing"
+)
+
+type config struct {
+	EnvTestVar    string `mapstructure:"envTestVar"`
+	YamlTestVar   string `mapstructure:"yamlTestVar"`
+	SecretTestVar string `mapstructure:"secretTestVar"`
+	Nested        nested `mapstructure:"nested"`
+}
+
+type nested struct {
+	Val1 string `mapstructure:"val1"`
+	Val2 int    `mapstructure:"val2"`
+	Val3 bool   `mapstructure:"val3"`
+}
+
+func TestLoadConfig(t *testing.T) {
+	// TODO: Find a way to replace project id from config file.
+	testCases := []struct {
+		name       string
+		configName string
+		envName    string
+		want       *config
+		wantErr    bool
+	}{
+		{
+			name:       "should load simple config",
+			configName: "testdata/config.yaml",
+			envName:    "testdata/test.env",
+			want: &config{
+				EnvTestVar:    "EnvVarValue 1235543",
+				YamlTestVar:   "Yaml Test",
+				SecretTestVar: "test-value",
+				Nested: nested{
+					Val1: "test",
+					Val2: 2,
+					Val3: true,
+				},
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.name, func(t *testing.T) {
+			got := &config{}
+
+			if tc.envName != "" {
+				LoadEnv(tc.envName)
+			}
+
+			err := LoadConfig(tc.configName, got)
+			if tc.wantErr && err == nil {
+				t.Errorf("expected error but got nil")
+				return
+			}
+			if err != nil {
+				t.Errorf("expected error to be nil but, got error: %+v", err)
+				return
+			}
+
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("expected = %+v but, got = %+v", tc.want, got)
+				return
+			}
+		})
+	}
+}
